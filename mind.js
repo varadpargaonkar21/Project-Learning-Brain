01e534922a0f93d19c46b4fb451b816c6c75fbfa
Varad Pargaonkar <117361138+varadpargaonkar21@users.noreply.github.com>
2022-12-15 12:59:42 +0530

    Add files via upload

---
+++ mind.js
@@ -0,0 +1,121 @@
+function medulaoblongeta(){
+
+    window.open('https://en.wikipedia.org/wiki/Medulla_oblongata');
+}
+
+function spinalcord(){
+
+    window.open('https://en.wikipedia.org/wiki/Spinal_cord');
+
+}
+
+function fourthventricle() {
+
+    window.open('https://en.wikipedia.org/wiki/Fourth_ventricle');
+
+}
+
+function cerebellum(){
+
+    window.open('https://en.wikipedia.org/wiki/Cerebellum');
+
+}
+
+function pinealgland(){
+
+    window.open('https://en.wikipedia.org/wiki/Pineal_gland');
+
+}
+
+function Occipitallobe(){
+
+    window.open('https://en.wikipedia.org/wiki/Occipital_lobe');
+
+}
+
+function Parietooccipitalsulcus(){
+
+    window.open('https://en.wikipedia.org/wiki/Parieto-occipital_sulcus');
+
+}
+
+function Parietallobe(){
+
+    window.open('https://en.wikipedia.org/wiki/Parietal_lobe');
+
+}
+
+function cerebralsulcus(){
+
+    window.open('https://en.wikipedia.org/wiki/Sulcus_(neuroanatomy)#:~:text=In%20neuroanatomy%2C%20a%20sulcus%20(Latin,sulci%20are%20usually%20called%20fissures.');
+
+}
+
+function Cerebralcortex(){
+
+    window.open('https://en.wikipedia.org/wiki/Cerebral_cortex');
+
+}
+
+function Frontallobe(){
+
+    window.open('https://en.wikipedia.org/wiki/Frontal_lobe');
+
+}
+
+function Corpuscallosum(){
+
+    window.open('https://en.wikipedia.org/wiki/Corpus_callosum');
+
+}
+
+function Interthalamicadhesion(){
+
+    window.open('https://en.wikipedia.org/wiki/Interthalamic_adhesion');
+
+}
+
+function Thalamus(){
+
+    window.open('https://en.wikipedia.org/wiki/Thalamus');
+
+}
+
+function Hypothalamus(){
+
+    window.open('https://en.wikipedia.org/wiki/Hypothalamus');
+
+}
+
+function Pituitarygland(){
+
+    window.open('https://en.wikipedia.org/wiki/Pituitary_gland');
+
+}
+
+function Pons(){
+
+    window.open('https://en.wikipedia.org/wiki/Pons');
+
+}
+
+
+function Reticularactivatingsystem(){
+
+    window.open('https://en.wikipedia.org/wiki/Reticular_formation');
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
